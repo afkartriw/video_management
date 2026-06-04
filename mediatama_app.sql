152b362b3a4b75f51d2ac242d1f1017b2cf31983
Muhammad Afkar Triwardana <116624209+afkartriw@users.noreply.github.com>
2026-06-05 06:32:18 +0700

    Add files via upload
    upload sql

---
+++ mediatama_app.sql
@@ -0,0 +1,262 @@
+﻿# Host: 127.0.0.1  (Version 5.5.5-10.4.32-MariaDB)
+# Date: 2026-06-05 06:31:24
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "cache"
+#
+
+DROP TABLE IF EXISTS `cache`;
+CREATE TABLE `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` bigint(20) NOT NULL,
+  PRIMARY KEY (`key`),
+  KEY `cache_expiration_index` (`expiration`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "cache"
+#
+
+
+#
+# Structure for table "cache_locks"
+#
+
+DROP TABLE IF EXISTS `cache_locks`;
+CREATE TABLE `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` bigint(20) NOT NULL,
+  PRIMARY KEY (`key`),
+  KEY `cache_locks_expiration_index` (`expiration`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "cache_locks"
+#
+
+
+#
+# Structure for table "failed_jobs"
+#
+
+DROP TABLE IF EXISTS `failed_jobs`;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) NOT NULL,
+  `connection` varchar(255) NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`),
+  KEY `failed_jobs_connection_queue_failed_at_index` (`connection`,`queue`,`failed_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "failed_jobs"
+#
+
+
+#
+# Structure for table "job_batches"
+#
+
+DROP TABLE IF EXISTS `job_batches`;
+CREATE TABLE `job_batches` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "job_batches"
+#
+
+
+#
+# Structure for table "jobs"
+#
+
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE `jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` smallint(5) unsigned NOT NULL,
+  `reserved_at` int(10) unsigned DEFAULT NULL,
+  `available_at` int(10) unsigned NOT NULL,
+  `created_at` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `jobs_queue_index` (`queue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "jobs"
+#
+
+
+#
+# Structure for table "migrations"
+#
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "migrations"
+#
+
+INSERT INTO `migrations` VALUES (1,'0001_01_01_000000_create_users_table',1),(2,'0001_01_01_000001_create_cache_table',1),(3,'0001_01_01_000002_create_jobs_table',1);
+
+#
+# Structure for table "password_reset_tokens"
+#
+
+DROP TABLE IF EXISTS `password_reset_tokens`;
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "password_reset_tokens"
+#
+
+
+#
+# Structure for table "request"
+#
+
+DROP TABLE IF EXISTS `request`;
+CREATE TABLE `request` (
+  `id_req` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cus_req` int(11) DEFAULT NULL,
+  `id_vid_req` int(11) DEFAULT NULL,
+  `pesan_req` varchar(255) DEFAULT NULL,
+  `tanggal_req` date DEFAULT NULL,
+  `status_req` varchar(255) DEFAULT NULL,
+  `mulai_akses_req` timestamp NULL DEFAULT NULL,
+  `selesai_akses_req` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_req`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+#
+# Data for table "request"
+#
+
+INSERT INTO `request` VALUES (1,2,1,NULL,NULL,'pending',NULL,NULL,NULL,NULL),(2,2,4,'tolong di acc',NULL,'pending',NULL,NULL,'2026-06-04 15:36:20','2026-06-04 15:36:20'),(3,2,4,'ppp','2026-06-04','acc','2026-06-04 23:08:00','2026-06-05 23:08:00','2026-06-04 15:40:42','2026-06-04 16:08:52'),(4,2,6,'bang acc dong','2026-06-04','acc','2026-06-04 23:30:00','2026-06-06 23:42:00','2026-06-04 16:40:23','2026-06-04 16:42:44');
+
+#
+# Structure for table "sessions"
+#
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sessions_user_id_index` (`user_id`),
+  KEY `sessions_last_activity_index` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "sessions"
+#
+
+INSERT INTO `sessions` VALUES ('4oBBMmZsvJvHBd7ADK1V8qP0Kah8LD0lYisIBQRE',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36','YTo0OntzOjY6Il90b2tlbiI7czo0MDoidkNSMllYNTZMTXpQMFp5NkRpSkh5cHczNGphZVVvbGpmYWRDQ2dmSyI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czoyOToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL3JlcXVlc3QiO31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2xvZ2luIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==',1780614612),('c2zqsB12aD2FtQySUYHAO3CFZWFtAhAHQoWziqAr',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36','YTo0OntzOjY6Il90b2tlbiI7czo0MDoibTY2WmsxcUZ1NlNVU0ppOGFidXBSSUIzaEVxc0dZdTRjUUhIc3Y1WiI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL3ZpZGVvIjt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC92aWRlbyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1780612315),('ib4mxaA8jncUZLCohIyLj7zvcHqE8eHXNeMwaQEl',2,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36','YTo1OntzOjY6Il90b2tlbiI7czo0MDoiM1ZlU0llZFRKejQwTHU4Q0hzM2Rqd2RzZlFqV3ViVjlzcmZFckxuNCI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Rhc2hib2FyZCI7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjI5OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvdmlkZW8vNiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7fQ==',1780614607),('xZDs0jStcrqydI3FI779ZCbLjY1Yf5Dp8YbOG8R0',1,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36','YTo1OntzOjY6Il90b2tlbiI7czo0MDoienZ6eXJlN3Ftc0U2RVBLNjlMbU5yY2hzNnpFMXE0cENYMXM2aFh3RyI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozMDoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2N1c3RvbWVyIjt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9jdXN0b21lciI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==',1780615455);
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id_users` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password_asli` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `status_users` varchar(255) DEFAULT 'aktif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_users`),
+  UNIQUE KEY `users_email_unique` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#
+# Data for table "users"
+#
+
+INSERT INTO `users` VALUES (1,'admin','admin','admin123','$2y$12$LsHwh1xysi06sbxcEkTEdO9ynPuJsdgYkJ3bD6F.vexNhLQI.RzAW','admin','aktif','2026-06-02 12:29:58','2026-06-02 12:29:58'),(2,'afkar','afkar','admin123','$2y$12$yALs5X66dLfMhrZ7j04nbOCBEuoM2JKykidlEYEDz.Zd/tXVaSq2W','customer','aktif','2026-06-04 06:40:04','2026-06-04 06:40:04');
+
+#
+# Structure for table "video"
+#
+
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video` (
+  `id_vid` int(11) NOT NULL AUTO_INCREMENT,
+  `judul_vid` varchar(255) DEFAULT NULL,
+  `thumbnail_vid` varchar(255) DEFAULT NULL,
+  `deskripsi_vid` longtext DEFAULT NULL,
+  `link_vid` varchar(255) DEFAULT NULL,
+  `tanggal_vid` date DEFAULT NULL,
+  `status_vid` varchar(255) DEFAULT 'aktif',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_vid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+#
+# Data for table "video"
+#
+
+
+#
+# Structure for table "video_detail"
+#
+
+DROP TABLE IF EXISTS `video_detail`;
+CREATE TABLE `video_detail` (
+  `id_vid_dt` int(11) NOT NULL AUTO_INCREMENT,
+  `id_vid_vid_dt` int(11) DEFAULT NULL,
+  `id_cus_vid_dt` int(11) DEFAULT NULL,
+  `komen_vid_dt` longtext DEFAULT NULL,
+  `like_vid_dt` int(11) DEFAULT NULL,
+  `dislike_vid_dt` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_vid_dt`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+#
+# Data for table "video_detail"
+#
+
